4ab9fa6dc801595f074ab203381a37ba39f83fad
zhr9909 <2580397082@qq.com>
2022-04-21 13:40:42 +0800

    网站只能处理一个列表项

--- functional_tests.py
+++ functional_tests.py
@@ -34,8 +34,18 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element(By.ID, 'id_list_table')
         rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows),
-                        "New to-do item did not appear in table")
+        self.assertIn('1: Buy peacock feathers',[row.text for row in rows])
+
+        # there is still a text box
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
 
         self.fail('Finish the test!')
 
