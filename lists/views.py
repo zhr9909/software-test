e8d25498c3efe382f0b422d469e4f42663b4b90c
zhr9909 <2580397082@qq.com>
2022-05-25 09:36:53 +0800

    add new list

--- lists/views.py
+++ lists/views.py
@@ -1,16 +1,21 @@
-from django.shortcuts import redirect,render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from lists.models import Item
 
 
 # Create your views here.
 def home_page(request):
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-only-list-in-the-world/')
+    # if request.method == 'POST':
+    #     Item.objects.create(text=request.POST['item_text'])
+    #     return redirect('/lists/the-only-list-in-the-world/')
     return render(request, 'home.html')
 
 
 def view_list(request):
     items = Item.objects.all()
-    return render(request, 'list.html', {'items':items})
+    return render(request, 'list.html', {'items': items})
+
+
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')
