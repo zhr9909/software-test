e5d9b09b2e08bcc9ac8a8472d39d9ba92031f63a
zhr9909 <2580397082@qq.com>
2022-05-26 15:34:49 +0800

    Use bootstrap to improve layout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,4 +1,4 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
@@ -9,7 +9,7 @@ import unittest
 MAX_WAIT = 10
 
 
-class NewVisitorTest(LiveServerTestCase):  # (1)
+class NewVisitorTest(StaticLiveServerTestCase):  # (1)
 
     def test_layout_and_styling(self):
         # eidth goes to the home_page
