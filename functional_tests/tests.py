108a9f2b5fdad3447847ed5fda6b75b54d12bbc7
zhr9909 <2580397082@qq.com>
2022-05-26 10:10:51 +0800

    multiple users tests

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,32 +1,27 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 from django.test import LiveServerTestCase
+
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-import unittest
 import time
+import unittest
 
 MAX_WAIT = 10
 
 
-class NewVisitorTest(LiveServerTestCase):
-    browser = webdriver.Firefox()
+class NewVisitorTest(LiveServerTestCase):  # (1)
 
-    def setup(self):
+    def setUp(self):  # (3)
         self.browser = webdriver.Firefox()
 
-    def tearDown(self):
+    def tearDown(self):  # (3)
         self.browser.quit()
 
     def wait_for_row_in_list_table(self, row_text):
         start_time = time.time()
         while True:
             try:
-                table = self.browser.find_element(By.ID, 'id_list_table')
+                table = self.browser.find_element_by_id('id_list_table')
                 rows = table.find_elements_by_tag_name('tr')
                 self.assertIn(row_text, [row.text for row in rows])
                 return
@@ -35,29 +30,48 @@ class NewVisitorTest(LiveServerTestCase):
                     raise e
                 time.sleep(0.5)
 
-    def test_can_start_a_list_for_one_user(self):
+    def test_can_start_a_list_for_one_user(self):  # (2)
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check out its homepage
+
         self.browser.get(self.live_server_url)
-        self.assertIn('To-Do', self.browser.title)
 
-        header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
-        self.assertIn('To-Do', header_text)
+        # She notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)  # (4)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)  # (4)
 
-        inputbox = self.browser.find_element(By.ID, 'id_new_item')
-        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a To-Do item')
-
+        # She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+        # She types "Buy peacock feathers" into a text box (Edith's hobby
+        # is tying fly-fishing lures)
         inputbox.send_keys('Buy peacock feathers')
+
+        # When she hits enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
-        # there is still a text box
-        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        # There is still a text box inviting her to add another item. She
+        # enters "Use peacock feathers to make a fly" (Edith is very methonical)
+        inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
 
+        # The page updates again, and now shows both items on her list
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
         self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        # Satisfied, she goes back to sleep
 
-        self.fail('Finish the test!')
+        # Edith wonders whether the site will remember her list. Then she sees
+        # that the site has generated a unique URL for her -- there is some
+        # explanatory text to that effect.
+        # self.fail('Finish the test!')
+        # She visits that URL - her to-do list is still there.
 
     def test_multiple_users_can_start_lists_at_different_urls(self):
         # Edith starts a new to-do list
@@ -81,13 +95,13 @@ class NewVisitorTest(LiveServerTestCase):
         # Francis visits the home page. There is no sign of Edith's
         # list
         self.browser.get(self.live_server_url)
-        page_text = self.browser.find_element(By.TAG_NAME, 'body').text
+        page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertNotIn('make a fly', page_text)
 
         # Francis starts a new list by entering a new item. He
         # is less interesting than Edith...
-        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Buy milk')
         inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy milk')
@@ -98,11 +112,11 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotEqual(francis_list_url, edith_list_url)
 
         # Again, there is no trace of Edith's list
-        page_text = self.browser.find_element(By.TAG_NAME, 'body').text
+        page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertIn('Buy milk', page_text)
 
-# if __name__ == '__main__':
-#    unittest.main(warnings='ignore')
+        # Satisfied, they both go back to sleep
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# if __name__=='__main__': #(6)
+#     unittest.main(warnings='ignore')
