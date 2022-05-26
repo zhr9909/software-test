07ee43b51d4513d8beefe2ebce8d05508f995d9c
zhr9909 <2580397082@qq.com>
2022-05-26 14:03:56 +0800

    first step of FT for layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -11,6 +11,22 @@ MAX_WAIT = 10
 
 class NewVisitorTest(LiveServerTestCase):  # (1)
 
+    def test_layout_and_styling(self):
+        # eidth goes to the home_page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # she notice the box is centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=10)
+
+        # she strats a new list and see the input is nicely centered there too
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=10)
+
     def setUp(self):  # (3)
         self.browser = webdriver.Firefox()
 
